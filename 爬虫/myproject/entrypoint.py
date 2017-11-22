fbf1c3ad64a794383a6b3f3d44adb697a505fc06
qiualu <1101938064@qq.com>
2017-11-22 11:23:12 +0800

    删除,整理

--- 爬虫/myproject/entrypoint.py
+++
@@ -1,3 +0,0 @@
-from scrapy.cmdline import execute
-# execute(['scrapy','crawl','dingdian']) #第三个参数是spider的名字
-execute(['scrapy','crawl','myproject'])
--- 爬虫/理论学习02/测试.py
+++
@@ -1,4 +0,0 @@
-import os
-base_data = os.path.abspath('02图片下载')
-
-print(base_data)
